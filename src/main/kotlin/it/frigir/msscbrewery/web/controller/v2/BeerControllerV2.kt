1764064390517fd09dc7e8ab2c3b2f3ad2129a2d
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-04-13 20:56:15 +0200

    bean validation

--- src/main/kotlin/it/frigir/msscbrewery/web/controller/v2/BeerControllerV2.kt
+++ src/main/kotlin/it/frigir/msscbrewery/web/controller/v2/BeerControllerV2.kt
@@ -3,13 +3,18 @@ package it.frigir.msscbrewery.web.controller.v2
 import it.frigir.msscbrewery.web.model.BeerDto
 import it.frigir.msscbrewery.web.model.v2.BeerDtoV2
 import it.frigir.msscbrewery.web.services.v2.BeerServiceV2
+import org.jetbrains.annotations.NotNull
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import java.util.function.Consumer
+import javax.validation.ConstraintViolationException
 
+@Validated
 @Controller
 @RequestMapping("/api/v2/beer")
 class BeerControllerV2(val beerService: BeerServiceV2) {
@@ -20,7 +25,7 @@ class BeerControllerV2(val beerService: BeerServiceV2) {
     }
 
     @PostMapping
-    fun handlePost(@RequestBody beerDto: BeerDtoV2): ResponseEntity<HttpHeaders> {
+    fun handlePost(@NotNull @RequestBody beerDto: BeerDtoV2): ResponseEntity<HttpHeaders> {
         val savedDto = beerService.saveBeer(beerDto)
         val headers = HttpHeaders()
         //TODO add host to url
@@ -29,15 +34,23 @@ class BeerControllerV2(val beerService: BeerServiceV2) {
     }
 
     @PutMapping("/{beerId}")
-    fun handleUpdate(@PathVariable("beerId") beerId: UUID, @RequestBody beerDto: BeerDtoV2): ResponseEntity<String> {
+    fun handleUpdate(@PathVariable("beerId") beerId: UUID, @NotNull @RequestBody beerDto: BeerDtoV2): ResponseEntity<String> {
         beerService.updateBeer(beerId, beerDto)
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun handleDelete(@PathVariable("beerId") beerId: UUID){
+    fun handleDelete(@PathVariable("beerId") beerId: UUID) {
         beerService.deleteBeer(beerId)
     }
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun validationExceptionHandler(e: ConstraintViolationException): ResponseEntity<List<String>> {
+        var errors = mutableListOf<String>()
+        e.constraintViolations.forEach(Consumer { errors.add((it.propertyPath + ": " + it.message).toString()) })
+        return ResponseEntity(errors, HttpStatus.BAD_REQUEST)
+    }
+
 
 }

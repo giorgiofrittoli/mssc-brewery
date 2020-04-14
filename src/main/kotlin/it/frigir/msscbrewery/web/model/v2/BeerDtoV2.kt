1db87d282e2e65898e23f30800bada9ac94487ce
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-04-14 21:26:42 +0200

    fix beerdtov2

--- src/main/kotlin/it/frigir/msscbrewery/web/model/v2/BeerDtoV2.kt
+++ src/main/kotlin/it/frigir/msscbrewery/web/model/v2/BeerDtoV2.kt
@@ -1,10 +1,20 @@
 package it.frigir.msscbrewery.web.model.v2
 
-import java.util.*
 
-class BeerDtoV2 {
-    lateinit var id: UUID
-    var name: String? = null
-    var style: BeerStyle? = null
-    var upc: Long? = null
-}
+import java.math.BigDecimal
+import java.time.OffsetDateTime
+import java.util.*
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Null
+import javax.validation.constraints.Positive
+
+data class BeerDtoV2(@Null val id: UUID? = null,
+                   @Null var version: Integer? = null,
+                   @Null val createDateTime: OffsetDateTime? = null,
+                   @Null val lastModified: OffsetDateTime? = null,
+                   @NotBlank val beerName: String,
+                   @NotNull val beerStyle: BeerStyle,
+                   @NotNull @Positive val upc: Long,
+                   @NotNull @Positive val price: BigDecimal,
+                   val quantityOnHands: Int = 0)
